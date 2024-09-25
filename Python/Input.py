25099b72c13950c4111a187e6548c2e2be0baaad
Sreynich-En <sreynich.en@student.cadt.edu.kh>
2024-09-25 23:43:24 +0700

    update
    hkgkg

--- Python/Input.py
+++ Python/Input.py
@@ -1,6 +1,6 @@
 
 number = [1, 2, 3, 4, 5, 6]
-string = "Hello my name is Nei tong."
+
 
 
 
@@ -8,4 +8,5 @@ string = "Hello my name is Nei tong."
 print("Output string and number:\n %s" % (string))
 for i in range (True):
     print(number[i])
+    
     
