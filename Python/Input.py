90c9849c4fc0dafb28c28565fc242c4c3c3a5efb
Nei tong <Hengneitong.keo@student.cadt.edu.kh>
2024-09-25 23:18:35 +0700

    Add files via upload

---
+++ Python/Input.py
@@ -0,0 +1,11 @@
+
+number = [1, 2, 3, 4, 5, 6]
+string = "Hello my name is Nei tong."
+
+
+
+#print
+print("Output string and number:\n %s" % (string))
+for i in range (True):
+    print(number[i])
+    
